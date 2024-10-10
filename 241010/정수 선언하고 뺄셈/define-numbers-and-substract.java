2a705b99154df4b82532a540bdda5eff0507e2fb
22-JWL <100843910+22-JWL@users.noreply.github.com>
2024-10-10 16:38:35 +0900

    Title: 정수 선언하고 뺄셈, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241010/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int firstInt = 97;
+        int secondInt = 13;
+        int mul = firstInt -secondInt;
+
+        System.out.println(firstInt + " - " + secondInt + " = " + mul);
+        // 여기에 코드를 작성해주세요.
+    }
+}
