8dce1de95f5d1329e7a13d5725853f1e0c561b41
22-JWL <100843910+22-JWL@users.noreply.github.com>
2024-10-10 16:43:17 +0900

    Title: 정수 선언하고 곱 출력, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241010/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+       int firstInt = 26;
+       int secondInt = 5;
+       int sub = firstInt * secondInt;
+
+       System.out.println(firstInt + " * " + secondInt + " = " + sub);
+    }
+}
