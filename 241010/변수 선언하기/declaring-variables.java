74c503236bfab0607ae59cddd4aa9c3ddd08858a
22-JWL <100843910+22-JWL@users.noreply.github.com>
2024-10-10 16:40:27 +0900

    Title: 변수 선언하기, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241010/변수 선언하기/declaring-variables.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int firstInt = 3;
+        String firstString = "C";
+        System.out.println(firstInt);
+        System.out.println(firstString);
+    }
+}
