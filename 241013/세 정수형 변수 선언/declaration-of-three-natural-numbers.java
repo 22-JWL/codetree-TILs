0955acd0640dc814b1e50dc710ccd6da29ccf4d7
22-JWL <100843910+22-JWL@users.noreply.github.com>
2024-10-13 19:25:39 +0900

    Title: 세 정수형 변수 선언, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241013/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        int a, b, c;
+
+        a= 7;
+        b= 23;
+        c=30;
+
+        System.out.println(a + " + " +b + " = " + c);
+    }
+}
