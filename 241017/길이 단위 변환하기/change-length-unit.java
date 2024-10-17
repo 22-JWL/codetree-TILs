0836e98c88d7c71bd2b748d2f8d53c8f8c9cbf08
22-JWL <100843910+22-JWL@users.noreply.github.com>
2024-10-17 16:47:10 +0900

    Title: 길이 단위 변환하기, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241017/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double oneFt = 30.48;
+        int oneMi =160934;
+
+        System.out.printf("%.1fft = %.1fcm \n", 9.2, oneFt*9.2);
+        System.out.printf("%.1fmi = %.1fcm", 1.3, oneMi*1.3);
+    }
+}
