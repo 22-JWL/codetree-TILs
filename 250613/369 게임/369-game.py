da09e8e5c6fcedd493c5943180090f123c88156d
22-JWL <100843910+22-JWL@users.noreply.github.com>
2025-06-13 15:20:12 +0900

    Title: 369 게임, Time: 81ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250613/369 게임/369-game.py
+++ 250613/369 게임/369-game.py
@@ -1,8 +1,12 @@
 N = int(input())
 
 for i in range (1, N+1):
-    if i%3==0 or (i%10)==3 or (i%10)==6 or (i%10)==9:
+    if i=30 or i = 60 or i=90:
         print(0, end=' ')
+    
+    elif i%3==0 or (i%10)==3 or (i%10)==6 or (i%10)==9:
+        print(0, end=' ')
+    
     else:
         print(i, end =' ')
     
