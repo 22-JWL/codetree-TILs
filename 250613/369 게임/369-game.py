5db42805f6c188678b1343e83355ca57650076f5
22-JWL <100843910+22-JWL@users.noreply.github.com>
2025-06-13 15:29:59 +0900

    Title: 369 게임, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 250613/369 게임/369-game.py
+++ 250613/369 게임/369-game.py
@@ -1,7 +1,14 @@
 N = int(input())
 
 for i in range (1, N+1):
-    if i%3==0 or (i%10)==3 or (i%10)==6 or (i%10)==9 or i==30 or i == 60 or i==90:
+    if i%3==0 or (i%10)==3 or (i%10)==6 or (i%10)==9:
+        print(0, end=' ')
+    elif i>30 and i<40:
+        print(0, end=' ')
+
+    elif i>60 and i<70:
+        print(0, end=' ')
+    elif i>90 and i<100:
         print(0, end=' ')
     else:
         print(i, end =' ')
