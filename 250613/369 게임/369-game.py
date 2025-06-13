11fbffa947b5926be3c242366269456020be219d
22-JWL <100843910+22-JWL@users.noreply.github.com>
2025-06-13 15:21:21 +0900

    Title: 369 게임, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250613/369 게임/369-game.py
+++ 250613/369 게임/369-game.py
@@ -1,9 +1,8 @@
 N = int(input())
 
 for i in range (1, N+1):
-    if i%3==0 or (i%10)==3 or (i%10)==6 or (i%10)==9 or i=30 or i = 60 or i=90:
+    if i%3==0 or (i%10)==3 or (i%10)==6 or (i%10)==9 or i==30 or i == 60 or i==90:
         print(0, end=' ')
-    
     else:
         print(i, end =' ')
     
