351c130f6f4594903c760ae81f9f64fada7a2255
22-JWL <100843910+22-JWL@users.noreply.github.com>
2025-07-02 20:12:43 +0900

    Title: 5의 배수의 합, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250702/5의 배수의 합/sum-of-specific-condition-multiple.py
+++ 250702/5의 배수의 합/sum-of-specific-condition-multiple.py
@@ -1,9 +1,14 @@
 inp = input()
-arr = inp.split()
-arr.sort()
-A = int(arr[0])
-B = int(arr[1])
+arr = list(map(int, inp.split()))
+sorted_arr = sorted(arr)  # 새로운 정렬된 배열 생성
+C = arr[0]
+D = arr[1]
+
+A = sorted_arr[0]
+B = sorted_arr[1]
 sum_val=0
+
+# print(f"{C}, {D}, {A}, {B}")
 
 
 for i in range(A, B+1):
